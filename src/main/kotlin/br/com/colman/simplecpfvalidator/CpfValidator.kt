74a1d54b863d4639045830ebeae4d7b7469fb431
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-05-24 11:26:55 -0300

    🚨 Tweak Detekt configuration

--- src/main/kotlin/br/com/colman/simplecpfvalidator/CpfValidator.kt
+++ src/main/kotlin/br/com/colman/simplecpfvalidator/CpfValidator.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+@file:Suppress("MagicNumber")
 package br.com.colman.simplecpfvalidator
 
 import kotlin.math.abs
