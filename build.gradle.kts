08a6d139a101e00653de6135e0d6665919d98c4d
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2024-10-19 18:00:03 -0300

    ✨ Switch to Kotlin Multiplatform and update publishing plugin
    Changed from JVM to Kotlin Multiplatform for broader compatibility,
    adding multiple targets including JS, iOS, and Linux. Replaced old Maven
     publishing setup with Vanniktech Maven Publish plugin to simplify the
     configuration and deployment process.
    
    Closes #7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
-  kotlin("jvm") version "2.0.21"
-  `maven-publish`
+  kotlin("multiplatform") version "2.0.21"
+  id("com.vanniktech.maven.publish") version "0.30.0"
   signing
   id("org.jetbrains.dokka") version "1.9.20"
   id("io.gitlab.arturbosch.detekt") version "1.23.7"
@@ -15,90 +15,70 @@ repositories {
   mavenCentral()
 }
 
-dependencies {
-  val kotestVersion = "5.9.1"
-  testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-  testImplementation("io.kotest:kotest-property:$kotestVersion")
-}
-
-tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = "1.8"
-}
-
 kotlin {
-  jvmToolchain(8)
+  applyDefaultHierarchyTemplate()
+
+  jvm()
+  js {
+    browser()
+    nodejs()
+  }
+
+  iosX64()
+  iosArm64()
+  iosSimulatorArm64()
+
+  macosX64()
+  macosArm64()
+
+  linuxX64()
+  linuxArm64()
+
+  sourceSets {
+    val jvmTest by getting {
+      dependencies {
+        val kotestVersion = "5.9.1"
+
+        implementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+        implementation("io.kotest:kotest-property:$kotestVersion")
+      }
+    }
+  }
 }
 
 tasks.withType<Test> {
   useJUnitPlatform()
 }
 
-val sourcesJar by tasks.registering(Jar::class) {
-  archiveClassifier.set("sources")
-  from(sourceSets.getByName("main").allSource)
-}
+mavenPublishing {
+  publishToMavenCentral(SonatypeHost.DEFAULT)
+  signAllPublications()
 
-val javadocJar by tasks.registering(Jar::class) {
-  dependsOn("dokkaHtml")
-  archiveClassifier.set("javadoc")
-  from("$buildDir/dokka")
-}
+  pom {
+    name.set("SimpleCpfValidator")
+    description.set("Simple CPF Validator")
+    url.set("https://www.github.com/LeoColman/SimpleCpfValidator")
 
-publishing {
-  repositories {
-    maven("https://oss.sonatype.org/service/local/staging/deploy/maven2") {
-      credentials {
-        username = System.getenv("OSSRH_USERNAME")
-        password = System.getenv("OSSRH_PASSWORD")
+
+    scm {
+      connection.set("scm:git:http://www.github.com/LeoColman/SimpleCpfValidator/")
+      developerConnection.set("scm:git:http://github.com/LeoColman/")
+      url.set("https://www.github.com/LeoColman/SimpleCpfValidator")
+    }
+
+    licenses {
+      license {
+        name.set("The Apache 2.0 License")
+        url.set("https://opensource.org/licenses/Apache-2.0")
+      }
+    }
+
+    developers {
+      developer {
+        id.set("LeoColman")
+        name.set("Leonardo Colman Lopes")
+        email.set("dev@leonardo.colman.com.br")
       }
     }
   }
-
-  publications {
-    register("mavenJava", MavenPublication::class) {
-      from(components["java"])
-      artifact(sourcesJar.get())
-      artifact(javadocJar.get())
-
-      pom {
-        name.set("SimpleCpfValidator")
-        description.set("Simple CPF Validator")
-        url.set("https://www.github.com/LeoColman/SimpleCpfValidator")
-
-
-        scm {
-          connection.set("scm:git:http://www.github.com/LeoColman/SimpleCpfValidator/")
-          developerConnection.set("scm:git:http://github.com/LeoColman/")
-          url.set("https://www.github.com/LeoColman/SimpleCpfValidator")
-        }
-
-        licenses {
-          license {
-            name.set("The Apache 2.0 License")
-            url.set("https://opensource.org/licenses/Apache-2.0")
-          }
-        }
-
-        developers {
-          developer {
-            id.set("LeoColman")
-            name.set("Leonardo Colman Lopes")
-            email.set("dev@leonardo.colman.com.br")
-          }
-        }
-      }
-    }
-  }
-}
-
-val signingKey: String? by project
-val signingPassword: String? by project
-
-signing {
-  useGpgCmd()
-  if (signingKey != null && signingPassword != null) {
-    useInMemoryPgpKeys(signingKey, signingPassword)
-  }
-
-  sign(publishing.publications["mavenJava"])
 }
